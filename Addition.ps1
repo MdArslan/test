f0de6b5549710e8c05e99b1a116df8ff0a887cb3
MdArslan <31751968+MdArslan@users.noreply.github.com>
2022-09-09 15:20:45 +0530

    Add files via upload

---
+++ Addition.ps1
@@ -0,0 +1,23 @@
+﻿Clear-Host
+<# Addition of two numbers
+min 2 numbers
+$a
+$b
+$sum = $a + $b
+
+#>
+
+<#Clear-Host
+$a = 10
+$b = 10
+
+$c = $a + $b
+Write-Host "Addition of $a and $b is $c"#>
+
+
+Clear-Host
+[int]$a = Read-Host "first number"
+[int]$b = Read-Host "Second number"
+[int]$d = Read-Host "Third number"
+$c = $a + $b +$d
+Write-Host "The addition of $a,$b and $d is $c"
